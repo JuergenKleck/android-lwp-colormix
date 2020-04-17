d90f178216a9abdbe3115f7e9698d88e4dca99fd
Jürgen Kleck <github.juergen@kleck.email>
2020-04-17 20:00:38 +0200

    Generic code improvements - Cleanup - Moving permissions
    Signed-off-by: Jürgen Kleck <github.juergen@kleck.email>

--- app/src/main/java/info/simplyapps/lwp/colormix/ColorMixSettingsFragment.java
+++ app/src/main/java/info/simplyapps/lwp/colormix/ColorMixSettingsFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SeekBarPreference;
 
